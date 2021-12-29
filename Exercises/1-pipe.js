64cb39119899bf261f22627fd47fd2af65f4fbc4
Hrystyna <yaskulskahrystyna@gmail.com>
2021-12-29 17:17:17 +0200

    Lab 6

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  for (const fn of fns) {
+    if (typeof(fn) !== 'function') {
+      throw new Error('Function expected!');
+    }
+  }
+  return x => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
